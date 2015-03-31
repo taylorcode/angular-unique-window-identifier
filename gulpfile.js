7df8b4b6f650650d225d514ec09a60d159ac7841
Taylor McIntyre <taylorm@icracked.com>
2015-03-30 18:17:42 -0700

    added documentation and a bundler

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,21 @@ var gulp = require('gulp'),
     jasmine = require('gulp-jasmine'),
     babel = require('gulp-babel'),
     sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat');
-
-gulp.task('dev:scripts', function() {
-    gulp.src('src/**/*.js') // throw vendor files in same file before souce files
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(concat('unique-window-identifier.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
-});
-
+    concat = require('gulp-concat'),
+    bundlerJs = require('bundler-js'),
+    packageFilename = 'unique-window-identifier'
  
-gulp.task('dev:test', function () {
+gulp.task('test', function () {
     return gulp.src('spec/test.js')
         .pipe(jasmine());
 });
 
-gulp.task('default', ['dev:scripts']);
+gulp.task('bundle', function () {
+    bundlerJs({
+        appScript: './src/' + packageFilename,
+        outputFile: packageFilename,
+        dest: 'dist'
+    });
+});
+
+gulp.task('default', ['bundle']);
--- src/unique-window-identifier.js
+++ src/unique-window-identifier.js
@@ -1,4 +1,13 @@
-class UniqueWindowIdentifier {
+/**
+ * @ngdoc service
+ * @name UniqueWindowIdentifier
+ *
+ * @description
+ * Creates a unique identifier on per-window basis that persists beyond a page reload.
+ *
+ */
+
+export default class UniqueWindowIdentifier {
 
   constructor($window) {
     this.$window = $window
@@ -6,16 +15,45 @@ class UniqueWindowIdentifier {
     this.nameKey = '__uniqueWindowIdentifier'
   }
 
+  /**
+   * @ngdoc method
+   * @private
+   * @name UniqueWindowIdentifier#_generateUniqueId
+   *
+   * @description
+   * Generates a unique id.
+   *
+   * @returns {Number} An integer unique id
+   */
   _generateUniqueId () {
     return Math.random().toString(36).slice(2)
   }
 
+  /**
+   * @ngdoc method
+   * @private
+   * @name UniqueWindowIdentifier#_setIdentifier
+   *
+   * @description
+   * Generates a unique id an assigns it to the windows name property
+   *
+   * @param {Object | undefined} nameObj An object that will be stringified with the unique id property
+   */
   _setIdentifier(nameObj) {
     let windowNameObj = nameObj || {}
     windowNameObj[this.nameKey] = this._generateUniqueId()
     this.currentWindow.name = JSON.stringify(windowNameObj)
   }
 
+  /**
+   * @ngdoc method
+   * @name UniqueWindowIdentifier#ensure
+   *
+   * @description
+   * When called, ensures that the window has a unique id. If the windows name property
+   * is already set, if it's already a serialized object, it will add or update the unique
+   * property on the object. If it's not an object, it will create one with the id property.
+   */
   ensure() {
     var windowNameObj
     // if windowProps.name is set and it's not a stringified object, convert it to it
@@ -37,10 +75,27 @@ class UniqueWindowIdentifier {
     }
   }
 
+  /**
+   * @ngdoc method
+   * @name UniqueWindowIdentifier#reset
+   *
+   * @description
+   * Regenerates and sets the identifier on the window.
+   */
   reset() {
     this._setIdentifier()
   }
 
+  /**
+   * @ngdoc method
+   * @name UniqueWindowIdentifier#get
+   *
+   * @description
+   * Picks the identifier from the parent object on the window's name property and
+   * returns it.
+   *
+   * @returns {String} The unique identifier
+   */
   get() {
     // return the identifier or undefined
     var identifier
@@ -51,7 +106,5 @@ class UniqueWindowIdentifier {
     }
     return identifier
   }
-  
-}
 
-angular.module('angularUniqueWindow', []).service('uniqueWindowIdentifier', UniqueWindowIdentifier)
+}
